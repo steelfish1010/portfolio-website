71b99d19fb5ac171b1b4a662ffafa2e9df226d25
amcombes <amcombes@penguin>
2022-06-07 13:40:10 +0100

    adds scrolling links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
@@ -18,11 +19,31 @@ const Navbar = () => {
 			{/* Menu */}
 
 			<ul className='hidden md:flex'>
-				<li>Home</li>
-				<li>About</li>
-				<li>Skills</li>
-				<li>Experience</li>
-				<li>Contact</li>
+				<li>
+					<Link to='home' smooth={true} duration={500}>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link to='about' smooth={true} duration={500}>
+						About
+					</Link>
+				</li>
+				<li>
+					<Link to='skills' smooth={true} duration={500}>
+						Skills
+					</Link>
+				</li>
+				<li>
+					<Link to='work' smooth={true} duration={500}>
+						Work
+					</Link>
+				</li>
+				<li>
+					<Link to='contact' smooth={true} duration={500}>
+						Contact
+					</Link>
+				</li>
 			</ul>
 
 			{/* Hamburger */}
@@ -38,11 +59,32 @@ const Navbar = () => {
 						: 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
 				}
 			>
-				<li className='py-6 text-4xl'>Home</li>
-				<li className='py-6 text-4xl'>About</li>
-				<li className='py-6 text-4xl'>Skills</li>
-				<li className='py-6 text-4xl'>Experience</li>
-				<li className='py-6 text-4xl'>Contact</li>
+				<li className='py-6 text-4xl'>
+					<Link to='home' onClick={handleClick} smooth={true} duration={500}>
+						Home
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link to='about' onClick={handleClick} smooth={true} duration={500}>
+						About
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link to='skills' onClick={handleClick} smooth={true} duration={500}>
+						Skills
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					{' '}
+					<Link to='work' onClick={handleClick} smooth={true} duration={500}>
+						Work
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link to='contact' onClick={handleClick} smooth={true} duration={500}>
+						Contact
+					</Link>
+				</li>
 			</ul>
 			{/* Social icons */}
 			<div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
